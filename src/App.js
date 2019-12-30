528dc86bd1764af7516d64db382adaa75d549764
Shubhra1028 <shubhra1028@gmail.com>
2019-12-30 22:04:41 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ReactiveBase, CategorySearch } from '@appbaseio/reactivesearch';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactiveBase
+          app="domain_data,bi_faq"
+          credentials="elastic:YjFLqw3h8lDmJUC3KhlLcxOU"
+          url="https://cors-anywhere.herokuapp.com/https://9d0cd00199224ccf8ffc6a672720c299.us-east-1.aws.found.io:9243/"
+          headers={{ "Content-Type": "application/x-ndjson" }}
+          transformResponse={(elasticsearchResponse) => {
+            console.log(elasticsearchResponse.hits.hits);
+            // this.setState({
+            //   suggestions : elasticsearchResponse.hits.hits
+            // }, ()=>{
+            //   return elasticsearchResponse.hits.hits;
+            // })
+            return elasticsearchResponse.hits.hits;
+          }}
+			>
+				<CategorySearch
+              componentId="SearchSensor"
+              dataField={["Questions^1.2", "Similar_Questions^1.0"]}
+              categoryField="s"
+          >
+				{/* <div>Hello ReactiveSearch!</div> */}
+        </CategorySearch>
+			</ReactiveBase>
     </div>
   );
 }
